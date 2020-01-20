9d078d07ac5f851e3a1d656e1e397f6900b15e59
Anthony <tineo.anthony@gmail.com>
2020-01-20 18:48:05 +0100

    update todo-list

--- src/app/todos/todo-list/todo-list.component.ts
+++ src/app/todos/todo-list/todo-list.component.ts
@@ -9,7 +9,9 @@ import { Todo } from '../todo.model';
 export class TodoListComponent implements OnInit {
   todos: Todo[] = [
     new Todo('Call James Gordon', 'TODO', '22/01/2020', 0),
-    new Todo('Call James Gordon', 'TODO', '22/01/2020', 0)
+    new Todo('Reply to Juste Leblanc', 'IN PROGRESS', '24/01/2020', 1),
+    new Todo('Go for a walk with John Doe', 'DONE', '19/01/2020', 2),
+    new Todo('Meet Batman', 'CANCELED', '12/01/2020', 3)
   ];
 
   constructor() { }
